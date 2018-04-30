3079d66922586bdd2fd6b997b1655f9818206e10
Nanette Girzi <ngirzi@gmail.com>
2018-04-30 16:39:31 -0700

    follow lesson

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCLTAlrHP7_ezHsuYfbKfgWHPjSUo0CMWE';
 
 
@@ -12,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = { videos: [] };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos });
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
